a9ad4a7337d8e99ad8f4a85b43f6c7d5a23759d8
Tyler Mulligan <tyler.mulligan.ctr@adlnet.gov>
2015-02-15 06:27:27 -0500

    put jQuery page ready inside require callback, removed most CDNs

--- js/app.js
+++ js/app.js
@@ -1,22 +1,14 @@
 require.config({
     baseUrl: 'js/lib',
     
-  /*<script type="text/javascript" language="javascript" src=""></script>
-  <script type="text/javascript" language="javascript" src="js/moment.min.js"></script>
-  <script type="text/javascript" language="javascript" src="js/bootstrap-datetimepicker.min.js"></script>
-  <script type="text/javascript" language="javascript" src="js/bootstrap-growl.min.js"></script>
-  <script type="text/javascript" language="javascript" src="js/cryptojs_v3.1.2.min.js"></script>
-  <script type="text/javascript" language="javascript" src="js/xapiwrapper.min.js"></script>
-  <script type="text/javascript" language="javascript" src="js/scripts.js" class="init"></script>*/
-    
     paths: {
         jquery: 'jquery',
         datatables: 'jquery.dataTables.min',
         cookie: 'jquery.cookie',
-        dtbootstrap: 'http://cdn.datatables.net/plug-ins/f2c75b7247b/integration/bootstrap/3/dataTables.bootstrap',
+        dtbootstrap: 'dataTables.bootstrap',
         transition: 'transition',
         collapse: 'collapse',
-        prettify: 'https://cdnjs.cloudflare.com/ajax/libs/prettify/r298/prettify.min',
+        prettify: 'prettify.min',
         moment: 'moment.min',
         datetimepicker: 'bootstrap-datetimepicker.min',
         growl: 'bootstrap-growl.min',
@@ -44,8 +36,7 @@ require.config({
         growl: {
             deps: ['dtbootstrap']
         }
-    },
-    waitSeconds: 15
+    }
 });
 
 requirejs(['../app/main']);
